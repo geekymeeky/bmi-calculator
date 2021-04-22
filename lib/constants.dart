b50839c65a16b66d71691d0ec28ff624c3f5a297
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-04-22 16:29:12 +0530

    Added slider for height input

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,3 +8,7 @@ const kLabelTextStyle = TextStyle(
   fontSize: 20.0,
   color: Color(0xFF8D8E98),
 );
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
@@ -13,6 +14,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
+  int height = 100;
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +57,51 @@ class _InputPageState extends State<InputPage> {
         Expanded(
           child: ReusableCard(
             colour: kActiveCardColor,
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'HEIGHT',
+                  style: kLabelTextStyle,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: <Widget>[
+                    Text(
+                      height.toString(),
+                      style: kNumberTextStyle,
+                    ),
+                    Text(
+                      'cm',
+                      style: kLabelTextStyle,
+                    ),
+                  ],
+                ),
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    thumbColor: kBottomContainerColor,
+                    activeTrackColor: Colors.white,
+                    inactiveTrackColor: Color(0xFF8D8E98),
+                    overlayColor: kBottomContainerColor.withAlpha(0x29),
+                    thumbShape: RoundSliderThumbShape(
+                      enabledThumbRadius: 15.0,
+                    ),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                  ),
+                  child: Slider(
+                      value: height.toDouble(),
+                      min: 90.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      }),
+                )
+              ],
+            ),
           ),
         ),
         Expanded(
